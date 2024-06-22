879553faa74dac96c3a5ab159e531dd1a9332a95
Maksim Popov <exactlyy@gmail.com>
2024-06-22 00:19:40 +0000

    Debug=False

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
 ALLOWED_HOSTS = [
     '8000-maxgray7dev-djangoblog-g7qig9tsspa.ws.codeinstitute-ide.net',
